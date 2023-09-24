727067a712c5ff440797eaacf21bb314fe854183
HappyTanuki <rlaalstn120@icloud.com>
2023-09-24 21:44:02 +0900

    파일위치 이동

--- ai-backend/backend-test.sh
+++
@@ -1,1 +0,0 @@
-curl -d '{"user_location": "location", "user_disease": "", "user_allergy": "peanut", "user_vegan": "vegan", "food_name": "엽기떡볶이"}' -H "Content-Type: application/json" -X POST happytanuki.kr/api/ai/recommend_food
