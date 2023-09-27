b5dd75ea4b070033cb72f62362b707528b8a0e4c
HappyTanuki <rlaalstn120@icloud.com>
2023-09-27 12:10:31 +0900

    심볼릭 링크 추가

--- main.py
+++ main.py
@@ -2,17 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 app = FastAPI()
 
-<<<<<<< HEAD
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/test")
-async def test():
-=======
 @app.get("/api/test")
 async def root():
->>>>>>> main
     return {"message": "test"}
 
 if __name__ == "__main__":
