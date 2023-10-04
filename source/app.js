647dd90ec34766de7b64a80537d70a3c7d357f78
HappyTanuki <rlaalstn120@icloud.com>
2023-10-04 14:00:32 +0900

    저장

--- source/app.js
+++ source/app.js
@@ -2,14 +2,12 @@ import React, { useCallback, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 
-const userDisease = createRoot(document.getElementById("userDisease"));
-const userAllergy = createRoot(document.getElementById("userAllergy"));
-const userReligion = createRoot(document.getElementById("userReligion"));
-const myButton = createRoot(document.getElementById("myButton"));
+const root = createRoot(document.getElementById("root"));
+// const userDataForm = createRoot(document.getElementById("ReactApp"));
+// const recommandedFoodCards = createRoot(document.getElementById("FoodCards"));
 
 function AddDiseaseOptions() {
     return <>
-        <option>없음</option>
         <option>당뇨</option>
         <option>비만</option>
         <option>셀리악(Celiac)</option>
@@ -23,7 +21,6 @@ function AddDiseaseOptions() {
 
 function AddAllergyOptions() {
     return <>
-        <option>없음</option>
         <option>알류</option>
         <option>참깨</option>
         <option>우유</option>
@@ -38,7 +35,6 @@ function AddAllergyOptions() {
 
 function AddReligionOptions() {
     return <>
-        <option>없음</option>
         <option>이슬람교</option>
         <option>유대교</option>
         <option>불교</option>
@@ -49,6 +45,7 @@ function AddReligionOptions() {
 
 function Geolocation_Callback(position) {
     window.alert(position.coords.latitude + ' ' + position.coords.longitude);
+    document.getElementById("FoodCards").innerHTML=position.coords.latitude + ', ' + position.coords.longitude;
 }
 
 function MyButton() {
@@ -57,10 +54,69 @@ function MyButton() {
         navigator.geolocation.getCurrentPosition(Geolocation_Callback);
     }
 
-    return <button className="btn btn-primary" onClick={handleOnClick}>추천받기</button>;
+    return <button className="btn btn-primary w-25" onClick={handleOnClick}>추천받기</button>;
 }
 
-userDisease.render(<AddDiseaseOptions />);
-userAllergy.render(<AddAllergyOptions />);
-userReligion.render(<AddReligionOptions />);
-myButton.render(<MyButton />);
+function CreateUserDataForm() {
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        return;
+    }
+
+    return (
+    <div className="container">
+        <form id="userData" className="p-2 bg-secondary text-center" onSubmit={HandleSubmit}>
+        <div className="row my-2">
+            <div className="col">
+                <h3>질병여부</h3>
+                <select id="userDisease">
+                    <option value="0">없음</option>
+                    <AddDiseaseOptions />
+                </select>
+            </div>
+            <div className="col">
+                <h3>알러지 여부</h3>
+                <select id="userAllergy">
+                    <option value="0">없음</option>
+                    <AddAllergyOptions />
+                </select>
+            </div>
+            <div className="col">
+                <h3>종교 여부</h3>
+                <select id="userReligion">
+                    <option value="0">없음</option>
+                    <AddReligionOptions />
+                </select>
+            </div>
+            <div className="col">
+                <h3>비건 여부</h3>
+                <select id="userVegan">
+                <option>아니오</option>
+                <option>예</option>
+                </select>
+            </div>
+        </div>
+        <div className="row justify-content-center">
+            <MyButton />
+        </div>
+        </form>
+    </div> );
+}
+
+function HandleFoodCards() {
+    return <>
+        <div id="FoodCards" className="vw-100 p-3 bg-info">test</div>
+    </>;
+}
+
+function RenderRoot() {
+    return <>
+        <CreateUserDataForm />
+        <HandleFoodCards />
+    </>;
+}
+
+root.render(<RenderRoot />);
+
+// userDataForm.render(<CreateUserDataForm />);
+// recommandedFoodCards.render(<HandleFoodCards />);
